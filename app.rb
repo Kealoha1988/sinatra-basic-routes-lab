f942f8c33031e9552fd16568c386e3ebe2d9004b
Kealoha1988 <kealohaabe1988@gmail.com>
2020-12-21 18:39:33 -0500

    commit tree a55a2a77dc8c183950db6c3f6f8dd69e7fd4c728

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kealoha Abe"
+  end
+
+  get '/hometown' do
+    "My hometown is Greensboro North Carolina"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is Here Comes The Sun"
+  end
 end
